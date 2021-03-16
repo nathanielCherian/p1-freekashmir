d3dc337f2aca9af161292d4c1a004befb5044e2b
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-03-16 14:25:08 -0700

    basic react

---
+++ homepage/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import logo from './logo.svg';
+import './scss/main.scss'
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <Navbar/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ homepage/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../scss/components/navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="nav-item logo-text">
+                    <span>Name</span>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
