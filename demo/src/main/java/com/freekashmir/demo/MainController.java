ad964d58c8ea4d8559ca7a06792ea49630dd655a
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-03-18 12:30:26 -0700

    fix file serving

--- demo/src/main/java/com/freekashmir/demo/MainController.java
+++ demo/src/main/java/com/freekashmir/demo/MainController.java
@@ -2,9 +2,20 @@ package com.freekashmir.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String homepage(){
+        return "index.html";
+    }
+
+    @GetMapping("/get-started")
+    public String getstarted(){
+        return "get-started.html";
+    }
 
 }
